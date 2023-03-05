2d2b890129417095c67498e158be860940a6fcc5
Henry Rincon <henryrinconsantana@gmail.com>
2023-03-05 10:37:03 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor() {
+    const tituloMhcp = <HTMLTitleElement>document.getElementById('titulo-mhcp');
+    tituloMhcp.textContent = environment.titleApp;
+  }
 }
